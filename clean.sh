af1eef33f8d8596e6ae97ddccaa46dc0bbac02b2
Florent Chuffart <florent.chuffart@gmail.com>
2022-12-06 17:19:40 +0100

    Refactoring

--- clean.sh
+++ clean.sh
@@ -1,10 +1,10 @@
 rm -Rf                          \
 bundle                          \
-Bundle_generator_-_cmd.html     \
+generate_bundle.html            \
 bundle.yaml                     \
 configuration.rds               \
 data_test.rds                   \
 data_train.rds                  \
 data_truth.rds                  \
 generate_data.html              \
-
+scoring_program/config.R        \
