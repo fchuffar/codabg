c687f02021de5e8eaf99a6330417585b80d25fdb
Florent Chuffart <florent.chuffart@gmail.com>
2022-12-06 17:15:03 +0100

    Adding clean.sf file

---
+++ clean.sh
@@ -0,0 +1,10 @@
+rm -Rf                          \
+bundle                          \
+Bundle_generator_-_cmd.html     \
+bundle.yaml                     \
+configuration.rds               \
+data_test.rds                   \
+data_train.rds                  \
+data_truth.rds                  \
+generate_data.html              \
+
